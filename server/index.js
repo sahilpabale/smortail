0291d4f0f399dbac8a092d9eb6f59a9fa884e821
sahilpabale <dev.sahilpabale@gmail.com>
2023-01-16 20:26:42 +0530

    completion api done

--- server/index.js
+++ server/index.js
@@ -47,6 +47,6 @@ app.post("/completion", async (req, res) => {
   }
 });
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log("Server running on port 3000");
 });
