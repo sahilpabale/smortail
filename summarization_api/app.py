de0870994d8dd3709b4ad7352ff92621ec5184f1
sahilpabale <dev.sahilpabale@gmail.com>
2023-04-04 17:16:52 +0530

    added summarization api

---
+++ summarization_api/app.py
@@ -0,0 +1,53 @@
+from flask import Flask, request, jsonify
+from newspaper import Article
+from newspaper import Config
+import spacy
+from spacy.lang.en.stop_words import STOP_WORDS
+from string import punctuation
+from heapq import nlargest
+
+app = Flask(__name__)
+
+def summarize(text, per):
+    nlp = spacy.load('en_core_web_sm')
+    doc= nlp(text)
+    tokens=[token.text for token in doc]
+    word_frequencies={}
+    for word in doc:
+        if word.text.lower() not in list(STOP_WORDS):
+            if word.text.lower() not in punctuation:
+                if word.text not in word_frequencies.keys():
+                    word_frequencies[word.text] = 1
+                else:
+                    word_frequencies[word.text] += 1
+    max_frequency=max(word_frequencies.values())
+    for word in word_frequencies.keys():
+        word_frequencies[word]=word_frequencies[word]/max_frequency
+    sentence_tokens= [sent for sent in doc.sents]
+    sentence_scores = {}
+    for sent in sentence_tokens:
+        for word in sent:
+            if word.text.lower() in word_frequencies.keys():
+                if sent not in sentence_scores.keys():                            
+                    sentence_scores[sent]=word_frequencies[word.text.lower()]
+                else:
+                    sentence_scores[sent]+=word_frequencies[word.text.lower()]
+    select_length=int(len(sentence_tokens)*per)
+    summary=nlargest(select_length, sentence_scores,key=sentence_scores.get)
+    final_summary=[word.text for word in summary]
+    summary=''.join(final_summary)
+    return summary
+
+@app.route("/api/summarize", methods=["POST"])
+def api():
+    data = request.get_json()
+    url = data["url"].strip()
+    user_agent = request.headers.get('User-Agent')
+    article = Article(url)
+    article.download()
+    article.parse()
+    return {
+        "success": True,
+        "article": article.text,
+        "summary": summarize(article.text, 0.1)
+    }
